fd519ca49e03aca841961c167ef2b65397c74805
jakeisadev <starowicz@protonmail.com>
2022-04-28 09:43:12 -0400

    input styling inserted, attempting to find minimum required input regex for js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function myZipCode() {
     var str = document.getElementById("zip").value;
     var zipCodeAlert = document.getElementById("zipcode-alert");
-    if(!((/^[0-9]{6}$/.test(str)))) {
+    if(!((/^[0-9]{5}$/.test(str)))) {
         zipCodeAlert.style.display="block";
-        zipCodeAlert.innerHTML="Zip Code must be only 6 digits";
+        zipCodeAlert.innerHTML="Zip Code must be 5 digits";
         zipCodeFlag = false;
-        return false;
     } else {
         zipCodeAlert.style.display="none";
         zipCodeFlag = true;
